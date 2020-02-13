4270bff83b048cee2deb5bc50a24479a09021046
Josh Comeau <joshwcomeau@gmail.com>
2020-02-13 17:29:23 -0500

    Test

---
+++ functions/hello.js
@@ -0,0 +1,8 @@
+exports.handler = async (ev, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      hello: "world",
+    }),
+  }
+}
--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,19 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const { ncp } = require("ncp")
+
+ncp.limit = 16
 
 // You can delete this file if you're not using it
+exports.onPostBuild = async () => {
+  return new Promise((resolve, reject) => {
+    ncp("./functions", "./public/functions", err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
